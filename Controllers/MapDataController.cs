43b80fd4d8baf5fe4ea9054bad09c17d922a0b66
Baris Can Soy <barcansoy@gmail.com>
2019-08-17 15:20:45 +0300

    added swagger and made the dll reference

---
+++ Controllers/MapDataController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LakeAreaService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TestTask.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MapDataController : Controller
+    {
+        /// <summary>
+        /// Builds the map object, finds the area of lakes(water squares that have an adjacent water square)
+        /// </summary>
+        /// <returns>total area of lakes</returns>
+        [HttpGet]
+        public ActionResult<int> LakeArea()
+        {
+            var mapBuilder = new MapBuilder();
+            var mapAnalyzer = new MapAnalyzer();
+
+            var map = mapBuilder.BuildMap();
+            var areas = mapAnalyzer.FindAdjacentRows(map);
+
+            return areas[0];
+        }
+
+        /// <summary>
+        /// Builds the map object, finds the area of lakes(water squares that have an adjacent water square)
+        /// </summary>
+        /// <returns>total area of waters</returns>
+        [HttpGet]
+        public ActionResult<int> WaterArea()
+        {
+            var mapBuilder = new MapBuilder();
+            var mapAnalyzer = new MapAnalyzer();
+
+            var map = mapBuilder.BuildMap();
+            var areas = mapAnalyzer.FindAdjacentRows(map);
+
+            return areas[1];
+        }
+
+        /// <summary>
+        /// Builds the map object.
+        /// </summary>
+        /// <returns>Map object JSON</returns>
+        [HttpGet]
+        public ActionResult Map()
+        {
+            var mapBuilder = new MapBuilder();
+            var map = mapBuilder.BuildMap();
+
+            return Json(map);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using LakeAreaService.Landscapes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TestTask
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IMap, Map>();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Map Data Api",
+                    Version = "v1",
+                    Description = "An API to get the map related data from the Resources folder",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Baris Can Soy",
+                        Email = "barcansoy@gmail.com"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            //Use api/*controller name*/*action name*
+            app.UseMvc();
+        }
+    }
+}
+//config => { config.MapRoute("defaultRoute", "api/{controller}/{action}"); }
