8c5be714595e1cd60079041e14bb31cbf82861f4
Baris Can Soy <barcansoy@gmail.com>
2019-08-18 03:14:05 +0300

    Added post operations in addition to file

--- Controllers/MapDataController.cs
+++ Controllers/MapDataController.cs
@@ -1,4 +1,5 @@
-﻿using LakeAreaService;
+﻿using System.Text;
+using LakeAreaService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TestTask.Controllers
@@ -8,7 +9,7 @@ namespace TestTask.Controllers
     public class MapDataController : Controller
     {
         /// <summary>
-        /// Builds the map object, finds the area of lakes(water squares that have an adjacent water square)
+        /// Builds the map object from input file, finds the area of lakes(water squares that have an adjacent water square) and returns the area of lakes.
         /// </summary>
         /// <returns>total area of lakes</returns>
         /// <response code="200">Returns the lake surface area</response>
@@ -28,7 +29,7 @@ namespace TestTask.Controllers
         }
 
         /// <summary>
-        /// Builds the map object, finds the area of waters
+        /// Builds the map object from input file, finds the area of waters and returns the area of waters.
         /// </summary>
         /// <returns>total area of waters</returns>
         /// <response code="200">Returns the water surface area</response>
@@ -48,7 +49,7 @@ namespace TestTask.Controllers
         }
 
         /// <summary>
-        /// Builds the map object.
+        /// Builds the map object from input file and returns it.
         /// </summary>
         /// <returns>Map object JSON</returns>
         /// <response code="200">Returns the Map object JSON</response>
@@ -62,6 +63,68 @@ namespace TestTask.Controllers
             var map = mapBuilder.BuildMap();
 
             return Json(map);
+        }
+
+        /// <summary>
+        /// Takes the map data as input and returns the Map object JSON.
+        /// </summary>
+        /// <param name="mapData"></param>
+        /// <response code="200">Returns the Map object JSON</response>
+        /// <response code="400">If the item is null</response>  
+        [HttpPost("{mapData}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult LoadMapData(string mapData)
+        {
+            var mapBuilder = new MapBuilder();
+            var bytes = Encoding.ASCII.GetBytes(mapData);
+            var map = mapBuilder.BuildMap(bytes);
+
+            return Json(map);
+        }
+
+        /// <summary>
+        /// Takes the map data as input and returns the Lake(water squares that have an adjacent water square) area.
+        /// </summary>
+        /// <param name="mapData"></param>
+        /// <response code="200">Returns the lake surface area</response>
+        /// <response code="400">If the item is null</response>  
+        [HttpPost("{mapData}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<int> LoadLakeData(string mapData)
+        {
+            var mapBuilder = new MapBuilder();
+            var mapAnalyzer = new MapAnalyzer();
+
+            var bytes = Encoding.ASCII.GetBytes(mapData);
+            var map = mapBuilder.BuildMap(bytes);
+
+            var areas = mapAnalyzer.GetLakeAndWaterAreas(map);
+
+            return areas[0];
+        }
+
+        /// <summary>
+        /// Takes the map data as input and returns the Lake(water squares that have an adjacent water square) area.
+        /// </summary>
+        /// <param name="mapData"></param>
+        /// <response code="200">Returns the water surface area</response>
+        /// <response code="400">If the item is null</response>  
+        [HttpPost("{mapData}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<int> LoadWaterData(string mapData)
+        {
+            var mapBuilder = new MapBuilder();
+            var mapAnalyzer = new MapAnalyzer();
+
+            var bytes = Encoding.ASCII.GetBytes(mapData);
+            var map = mapBuilder.BuildMap(bytes);
+
+            var areas = mapAnalyzer.GetLakeAndWaterAreas(map);
+
+            return areas[1];
         }
     }
 }
